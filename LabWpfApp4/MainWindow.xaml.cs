1598c3e1eb0588dd1085add484fa9f4a9c943398
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-02-11 00:02:34 +0300

    Добавьте файлы проекта.

---
+++ LabWpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabWpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate.Text);
+            double sumDollar = Convert.ToDouble(sum.Text);
+            double resDollar = rateDollar * sumDollar;
+            resSum.Text = resDollar.ToString();
+
+
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            double i = 2.54;
+            double rateInch = Convert.ToDouble(rate1.Text);
+            double resInch = rateInch * i;
+            resSum1.Text = resInch.ToString();
+
+
+        }
+    }
+}
